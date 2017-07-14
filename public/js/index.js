aebb8230cd95c2ada5953021e23da0e6721efa8c
gsominsk <zoedberg@i.ua>
2017-07-14 11:47:06 +0300

    main page added with js

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,4 @@
 window.onload = function () {
-
-/*
-** ==========================================
-**          main-slider text fadeout
-** ==========================================
-*/
-    $('.active .carousel-caption').fadeTo("slow", 1)
-    $('#mainCarousel').on('slid.bs.carousel', function () {
-        $('.active .carousel-caption').fadeTo("slow", 1)
-    })
-    $('#mainCarousel').on('slide.bs.carousel', function () {
-        $('.active .carousel-caption').fadeTo("slow", 0)
-    })
+    var anchor = new Anchor;
+    var slider = new Slider;
 }
