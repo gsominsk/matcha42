e75122aaaf20e05c26d2ba9d1c4927bda23818e0
gsominsk <zoedberg@i.ua>
2017-07-17 00:40:07 +0300

    chat, photo modal, friend page added

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,3 +1,16 @@
+class Chat {
+
+}
+
+class MainProfile {
+	constructor () {
+		this.textareaAutoresize = new TextareaAutoresize;
+	}
+}
+
 window.onload = function () {
-    var textarea = new TextareaAutoresize;
+	var mainProfile = new MainProfile;
+
+	var block = document.getElementsByClassName("chat-body")[0];
+	block.scrollTop = block.scrollHeight;
 }
